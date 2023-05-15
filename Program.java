54f696a0b80da0cfb139e8d0cf1cdbef7bdf1b2a
ViniciusAlvesGitHub <132413949+ViniciusAlvesGitHub@users.noreply.github.com>
2023-05-15 16:03:59 -0300

    primeiro commit estrutura de decisão

---
+++ Program.java
@@ -0,0 +1,86 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+		
+		/*Exercício1
+		System.out.println("Digite o primeiro número");
+		int valor1 = leia.nextInt();
+		
+		System.out.println("Digite o segundo número");
+		int valor2 = leia.nextInt();
+		
+		System.out.println("Digite o terceiro número");
+		int valor3 = leia.nextInt();
+		
+		int maiorNumero = valor1;
+		
+		if(valor2 > maiorNumero) {
+			maiorNumero = valor2;
+		}
+		
+		if(valor3 > maiorNumero) {
+			maiorNumero = valor3;
+		}
+		
+		System.out.println("O maior número é: " + maiorNumero);
+		
+		leia.close();
+		*/
+		
+		
+		
+		/*Exercício2
+		int numero;
+		System.out.println("Digite um ano para saber se é bissexto ou não:");
+		numero = leia.nextInt();
+		
+		if(numero%4 ==0) {
+			System.out.println("Ano bissexto!");
+		}
+		else {
+			System.out.println("Não é um ano bissexto!");
+		}
+		
+		leia.close();
+		*/
+		
+		
+		
+		/*Exercício3
+		System.out.println("Nota 1:");
+		double semestre1 = leia.nextDouble();
+		
+		System.out.println("Nota 2:");
+		double semestre2 = leia.nextDouble();
+		
+		System.out.println("Nota 3:");
+		double semestre3 = leia.nextDouble();
+		
+		System.out.println("Nota 4:");
+		double semestre4 = leia.nextDouble();
+		
+		double media = (semestre1+semestre2+semestre3+semestre4) / 4;
+		
+		System.out.println("Média: " + media);
+		
+		if (media >= 10) {
+            System.out.println("Aprovado com honras!");
+        } else if (media >= 7) {
+            System.out.println("Aprovado!");
+        } else if (media >= 6) {
+            System.out.println("Recuperação!");
+        } else {
+            System.out.println("Reprovado!");
+        }
+		*/
+      
+        
+	}
+
+}
